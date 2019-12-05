33e3442a29300be7d32f2eef27f30ed82d87d198
CBreakr <rob.notwicz@gmail.com>
2019-12-05 03:25:23 -0500

    swtched back to previous id matching style

--- api/removalUtilities/removeLists.js
+++ api/removalUtilities/removeLists.js
@@ -2,8 +2,6 @@
 const ListModel = require("../../models/ListModel");
 const removeTasks = require("./removeTasks");
 
-const objIdMatch = require("../../utilities/objectIdMatch");
-
 module.exports = removeLists = (projectId, user, next) => {
   console.log("remove lists", {projectId, user});
   ListModel.find({parentProject:projectId}, (err, lists) => {
@@ -18,7 +16,7 @@ module.exports = removeLists = (projectId, user, next) => {
       // remove the listaccess here, too
       user.listAccess = user.listAccess.filter(la => {
         // return la+"" !== list._id+"";
-        return !objIdMatch(la, list._id);
+        return !(la+"" === list._id+"");
       });
       user.save();
       removeTasks(list._id, next);
